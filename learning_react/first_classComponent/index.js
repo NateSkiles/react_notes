6df2b6cdb5d1602bfe3877d5b46d65039355d334
Nathan Skiles <nateskiles94@gmail.com>
2022-01-01 11:07:17 -0800

    first component init

---
+++ learning_react/first_classComponent/index.js
@@ -0,0 +1,13 @@
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Word!</h1>
+                <h2>Hello Word!</h2>
+                <h3>Hello Word!</h3>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
